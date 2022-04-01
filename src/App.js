7e3352249a52ac83e613c9c16c714309acbda302
Amariwest19 <Amariwest19@tamu.edu>
2022-04-01 18:08:21 -0500

    Added demo template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { FormBox } from './FormBox';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <FormBox />
+  );
+}
+
+export default App;
