d8d2c9c377affb796cf142afeb63fb911b0dfe20
jacobwde <jacobwde@tamu.edu>
2022-04-03 18:41:51 -0500

    Jacob and William did Peer Coding on Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import { FormBox } from "./FormBox";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {MyNavbar} from"./navbar";
 
 function App() {
-  return <FormBox />;
+  
+  return <>
+    <MyNavbar/> 
+    <FormBox />
+  </>;
 }
 
 export default App;
