1b7b6dd21aebcf4af82126353089d1af0376f930
Jacob Enerio <Jacob@enerio.net>
2022-04-17 14:02:15 -0500

    Made new branch for testing front end backend split

--- server/index.ts
+++ server/index.ts
@@ -2,21 +2,21 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
 
 // Allow communication with React Application
 const cors = require("cors");
 app.use(cors());
 
 // Database Connect
-mongoose.connect("mongodb+srv://Amariwest19:123@cluster0.enwzq.mongodb.net/WalrusDatabase?retryWrites=true&w=majority");
+// Needs to be reconfigured to whitelist ips
+//mongoose.connect("mongodb+srv://Amariwest19:123@cluster0.enwzq.mongodb.net/WalrusDatabase?retryWrites=true&w=majority");
 
 // Allows requests that have a body.
 app.use(express.json());
 
-// Server Output
-app.listen(3501, () => {
-    console.log("Server Running");
-});
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/public')));
 
 // Routes
 const test = require("./routes/Test.ts");
@@ -27,6 +27,17 @@ app.use("/careers", getStoredCareers);
 
 const gptRequests = require("./routes/GPT.ts");
 app.use("/gpt", gptRequests);
+
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req: any, res: any) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
+// Server Output
+app.listen(3501, () => {
+    console.log("Server Running");
+});
 
 export {}
 
