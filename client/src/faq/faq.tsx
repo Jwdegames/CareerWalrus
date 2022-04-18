0922370b0bc68ad747bd08c8e755b96195a6a63f
jacobwde <jacobwde@tamu.edu>
2022-04-18 11:30:37 -0500

    Began working on faq

---
+++ client/src/faq/faq.tsx
@@ -0,0 +1,13 @@
+
+
+/**
+ * Makes a FAQ for the landing page to use. 
+ */
+export function FAQ() {
+    return (
+        <>
+            <h2>
+                What is Career Walrus?
+            </h2>
+        </>);
+}
