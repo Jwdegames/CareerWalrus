1eaadbb7db0de487a09ae5436272311866650c31
pli5297 <paulli5297@gmail.com>
2022-04-20 12:28:16 -0500

    finished connecting local rest api

--- server/routes/OneStop.ts
+++ server/routes/OneStop.ts
@@ -8,25 +8,26 @@ Queries the list job function in Career OneStop's API
 */
 router.post("/getJobs", async (req : any, res : any) => {
     if (req) {
-        console.log(req);
+        console.log(req.body);
 
         const userId = "aTSpInK9SOGclcJ";
         const apiKey = "js9Ba1M/SNhb3mYbWwObvai+5SB+F6lEN+OHNu6va2Y5RHtev/RIwww0kvQC6QhaioDB/N5BBKclBfDNO1qRgw==";
-        const reqObj = JSON.parse(req);
 
         const uri = new UriBuilder;
         uri.schema = "https";
         uri.host = "api.careeronestop.org";
-        uri.pathSegments = ["v1", "jobsearch", userId, reqObj.keyword, reqObj.location, reqObj.radius, reqObj.sortColumns, reqObj.sortOrder, reqObj.startRecord, reqObj.pageSize, reqObj.days];
+        uri.setPath(`/v1/jobsearch/${userId}/${req.body.keyword}/${req.body.location}/${req.body.radius}/${req.body.sortColumns}/${req.body.sortOrder}/${req.body.startRecord}/${req.body.pageSize}/${req.body.days}`);
         
-        axios.post(uri.toString, { headers: { "Authorization": `Bearer ${apiKey}` } })
+        console.log(uri.toString());
+        axios.get(uri.toString(), { headers: { "Authorization": `Bearer ${apiKey}` } })
         .then((response: any) => {
             console.log(`Status Code: ${response.status}`);
-            console.log(response);
-            res.send(response)
+            console.log(response.status);
+            console.log(response.data);
+            res.send(response.data);
         })
         .catch((error: any) => {
-            console.log(error);
+            console.log(error.response.data);
         })
     } else {
         console.log("Empty request body for getJobs");
