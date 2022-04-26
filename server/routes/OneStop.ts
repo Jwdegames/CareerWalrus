d6377e637d6202ce44ba289988d9e7812da561d6
pli5297 <paulli5297@gmail.com>
2022-04-26 13:29:34 -0500

    initial code

--- server/routes/OneStop.ts
+++ server/routes/OneStop.ts
@@ -30,6 +30,33 @@ router.post("/getJobs", async (req : any, res : any) => {
     }
 });
 
+/*
+Queries the get salary function in Career OneStop's API. Returns a JSON object
+*/
+router.post("/getJobs", async (req : any, res : any) => {
+    if (req) {
+        const userId = "aTSpInK9SOGclcJ";
+        const apiKey = "js9Ba1M/SNhb3mYbWwObvai+5SB+F6lEN+OHNu6va2Y5RHtev/RIwww0kvQC6QhaioDB/N5BBKclBfDNO1qRgw==";
+
+        const uri = new UriBuilder;
+        uri.schema = "https";
+        uri.host = "api.careeronestop.org";
+        uri.setPath(`/v1/comparesalaries/${userId}/wage?keyword=${req.body.keyword}&location=${req.body.location}&enableMetaData=${req.body.enableMetaData}`);
+        
+        axios.get(uri.toString(), { headers: { "Authorization": `Bearer ${apiKey}` } })
+        .then((response: any) => {
+            console.log(`api.careeronestop.org Status Code: ${response.status}`);
+            console.log(response)
+            res.send(response.data);
+        })
+        .catch((error: any) => {
+            console.log(error.response.data);
+        })
+    } else {
+        console.log("Empty request body for getJobs");
+    }
+});
+
 // Export router to use in index.js
 module.exports = router;
 
