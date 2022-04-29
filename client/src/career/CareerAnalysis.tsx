78598bad2f2811b451ba9c46f2f644859ffc7b64
jacobwde <jacobwde@tamu.edu>
2022-04-29 12:10:26 -0500

    Began adding area codes for BLS. Also added return to job listing button

--- client/src/career/CareerAnalysis.tsx
+++ client/src/career/CareerAnalysis.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Button, Container, Row, Col} from 'reactstrap'
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { CABList } from "./CABList";
 import "./CareerAnalysis.css"
 import "./CABList"
@@ -30,16 +31,31 @@ export function CareerAnalysis() {
         setLSInput();
     }
     //setListingState(state[listingState]);
+
+    var navigator = useNavigate();
+    /**
+     * Loads the job listing elements (i.e. returns to the previous page).
+     * 
+     * @param e Event that called the function
+     */
+    function loadJobListing(e: React.MouseEvent) {
+        // Go to job listing page
+        navigator("../job", {state: { listingState: listingSelection }});
+    }
+
     return (
     <>
         <p>
-            We are currently showing job listings for {listingState}.
+            We are currently showing job listings for {listingState}. &nbsp; &nbsp; &nbsp;
+            <Button className = "ret-btn" onClick = {loadJobListing}>Return To Job Listing</Button> &nbsp; &nbsp; &nbsp;
+            Select An Area To See Cost Of Living: 
         </p>
         <Container>
             <Row>
                 <Col className = "col" sm={4}>
                     Test
                     <CABList input = {setListingSelection} input2 = {listingState}></CABList>
+
                 </Col>
                 
                 <Col className = "col" sm={8}>{listingSelection}</Col>
