6c86ce12c7fc85d0553af655dd16f1e38ef53004
william <williamsleepark@gmail.com>
2022-04-30 17:57:28 -0500

    Modified presets to have clear indicators on what to do.

--- client/src/FormBox.tsx
+++ client/src/FormBox.tsx
@@ -1,39 +1,52 @@
 import { Form, FormGroup, Label, Input, Card, CardBody, CardText } from "reactstrap";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import Axios from "axios";
 import { QuestionPresets } from "./question-presets/presets";
 
 export default function FormBox() {
   const [output, setOutput] = useState("");
   const [input, setInput] = useState("");
+  const [GPT3Cache, setGPT3Cache] = useState("");
 
-  useEffect(() => {
+  // Calls new GPT3 API request only when gpt3Input changes
+
+  const callGPT3 = () => {
     let inputprompt =
       "Use the following summary of an individual's interests to generate a list of comma-seperated engineering jobs that fits their professional and academic profile. Try to return engineering related careers. If the user enters a career related question, answer it.\n \n User Input: ";
-
-    if (input) {
-      let debouncer = setTimeout(() => {
-        Axios.post("/gpt/sendGPTPrompt", {
-          prompt: inputprompt + input + "/n",
-          temperature: 0.1,
-          max_tokens: 256,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-        }).then((response) => {
-          setOutput(response.data);
-        });
-        console.log(output);
-      }, 1000);
-      return () => {
-        clearTimeout(debouncer);
-      };
+    if (GPT3Cache) {
+      Axios.post("/gpt/sendGPTPrompt", {
+        prompt: inputprompt + GPT3Cache + "/n",
+        temperature: 0.1,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      }).then((response) => {
+        setOutput(response.data);
+      });
     }
-  }, [input]); // we only want to ask GPT3 for response when user enters text so we listen specifically to 'input'
+  };
+
+  useEffect(() => {
+    callGPT3();
+  }, [GPT3Cache]);
 
   // this tells any listeners to 'input' to update with 'input's new value passed in
   const handleChange = (e: any) => {
     setInput(e.target.value);
+  };
+
+  // used for presets to set textarea with clicked question and call gpt3
+  const presetCaller = (value: string) => {
+    setInput(value);
+    setGPT3Cache(value);
+  };
+
+  const handleKeyPress = (event: any) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      setGPT3Cache(input);
+    }
   };
 
   return (
@@ -44,13 +57,14 @@ export default function FormBox() {
             <FormGroup>
               <Label for="gpt-input">Give a basic description of your interests to get career options or ask a question. Try to be specific... </Label>
               <br />
-              <Input id="gpt-input" type="textarea" value={input} onChange={handleChange}></Input>
+              <Input id="gpt-input" type="textarea" value={input} onChange={handleChange} onKeyPress={handleKeyPress}></Input>
             </FormGroup>
           </Form>
+          <button onClick={() => setGPT3Cache(input)}>Enter</button>
         </CardBody>
       </Card>
       <p> {output} </p>
-      <QuestionPresets setInput={setInput} output={output} />
+      <QuestionPresets setInput={presetCaller} output={output} />
     </div>
   );
 }
