4fc7d554d72aa1e1aa73e7008d150a693be75ebb
jacobwde <jacobwde@tamu.edu>
2022-05-01 19:43:50 -0500

    Fixed positioning of magnifiying glass

--- client/src/magnifying/Magnifying.tsx
+++ client/src/magnifying/Magnifying.tsx
@@ -24,6 +24,7 @@ export function Magnify({AreaToZoom} : MagnifiedInterface) {
     const [mousePos, setMousePos] = useState([50,50]);
     let magRef: React.RefObject<HTMLDivElement> = React.createRef();
     let magAreaRef: React.RefObject<HTMLDivElement> = React.createRef();
+    let mainAreaRef: React.RefObject<HTMLDivElement> = React.createRef();
     console.log("Reload called");
     function getMousePos(e: React.MouseEvent) {
         console.log(e.clientX + " , " + e.clientY);
@@ -31,43 +32,45 @@ export function Magnify({AreaToZoom} : MagnifiedInterface) {
 
     function moveMagnify(e: React.MouseEvent) {
         let magElement = magRef.current!;
-        getMousePos(e);
         let magAreaElement = magAreaRef.current!;
+        let mainAreaElement = mainAreaRef.current!;
+        getMousePos(e);
+        //console.log("Mag window location originally was " + magElement.style.left + "," + magElement.style.top);
+        console.log("Main area size is " + mainAreaElement.offsetWidth + "," + mainAreaElement.offsetHeight);
         let midHeight = magElement.offsetHeight / 2;
         let midWidth = magElement.offsetWidth / 2;
         //console.log("Midheight is " + midHeight);
         let left = (e.clientX - 75) +"px";
-        let top = (e.clientY - 550) +"px";
+        let top = (e.clientY - mainAreaElement.offsetHeight - 75 + window.scrollY) +"px";
         let translation = "translate(" + left + "," + top + ")";
-        let areaTranslation = "translate(" + (-left) + "," + (-top) + ")";
+        // let areaTranslation = "translate(" + (mainAreaElement.offsetWidth/2) + "," + (0) + ")";
         console.log("Moving to " + translation);
         magElement.style.transform =  translation;
         magAreaElement.style.left = "0px";
         magAreaElement.style.top = "0px";
 
-        let xCoord = e.clientX;
-        let yCoord = (e.clientY + window.scrollY);
-        //console.log("Scrolling to " + xCoord + "," + yCoord);
+        let xCoord = e.clientX - 20;
+        let yCoord = (e.clientY - 20 + window.scrollY);
+        console.log("Scrolling to " + xCoord + "," + yCoord);
         magElement.scrollTo(xCoord*2, yCoord*2);
     }
 
     return (
-        <div id = "magnify" className = "magnifyable" onMouseMove={moveMagnify}>
-             <span className="original-inner">
-
-            <div className="original magnifyable" >
-                <div className="az-overly az-overlay"></div>
-                <span className="original-inner">
-                    {React.cloneElement(AreaToZoom)}
-                </span>
-            </div>
-
-            <div className="magnified windowed" ref = {magRef as React.RefObject<HTMLDivElement>}>
-                <div className="copy static" ref = {magAreaRef as React.RefObject<HTMLDivElement>}>
-                    <MagnifiedClone AreaToZoom = {AreaToZoom}/>
+        <div id = "magnify" className = "magnifyable" onMouseMove={moveMagnify} ref = {mainAreaRef}>
+            <span className="original-inner">
+                <div className="original magnifyable" >
+                    <span className="original-inner">
+                        {React.cloneElement(AreaToZoom)}
+                    </span>
                 </div>
-            </div>
 
+                <span className = "magnifyable">
+                    <div className="magnified windowed" ref = {magRef as React.RefObject<HTMLDivElement>}>
+                        <div className="copy static" ref = {magAreaRef as React.RefObject<HTMLDivElement>}>
+                            <MagnifiedClone AreaToZoom = {AreaToZoom}/>
+                        </div>
+                    </div>
+                </span>
             </span>
         </div>
     );
